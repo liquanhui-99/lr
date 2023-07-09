ab6d2119ab03d701936afba96703a430c1aab93a
liquanhui-99 <55009116+liquanhui-99@users.noreply.github.com>
2023-07-09 17:18:22 +0800

    feat: 编写AddRouter方法，完善测试用例

--- router.go
+++ router.go
@@ -1,5 +1,7 @@
 package lorm
 
+import "strings"
+
 // router 一个森林，每一种请求方式都是一颗树，
 // key是请求的方法，value是树的节点
 type router struct {
@@ -23,5 +25,34 @@ func newRouter() *router {
 
 // AddRouter 注册路由信息
 func (r *router) AddRouter(pattern, path string, handler HandleFunc) {
+	root, ok := r.trees[pattern]
+	if !ok {
+		root = &node{
+			path:     "/",
+			children: make(map[string]*node),
+		}
+		r.trees[pattern] = root
+	}
+	path = strings.TrimLeft(path, "/")
+	segs := strings.Split(path, "/")
+	for _, seg := range segs {
+		// 递归查找每一个子节点是否存在
+		children := root.childOrCreate(seg)
+		root = children
+	}
+	root.handler = handler
+}
 
+func (n *node) childOrCreate(seg string) *node {
+	if n.children == nil {
+		n.children = make(map[string]*node)
+	}
+	res, ok := n.children[seg]
+	if !ok {
+		res = &node{
+			path: seg,
+		}
+		n.children[seg] = res
+	}
+	return res
 }
